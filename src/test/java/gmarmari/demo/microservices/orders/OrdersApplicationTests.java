f37f31a98b121e0cdd8470d100400cb3909e550c
gmarmari <georgemarmaris87@gmail.com>
2022-12-09 09:37:34 +0100

    create a spring application

---
+++ src/test/java/gmarmari/demo/microservices/orders/OrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package gmarmari.demo.microservices.orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
