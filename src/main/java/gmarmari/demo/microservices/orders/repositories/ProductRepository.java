a474ce66c59087ba83a33a8de3b504dd2df6bfa3
naumovHellsCoder <114773698+naumovHellsCoder@users.noreply.github.com>
2022-12-12 15:02:07 +0300

    Update ProductRepository.java
    @Repository - this is a specialization of the @Component annotation, which is used to indicate that the class provides a mechanism for storing, retrieving, updating, deleting, and searching objects.

--- src/main/java/gmarmari/demo/microservices/orders/repositories/ProductRepository.java
+++ src/main/java/gmarmari/demo/microservices/orders/repositories/ProductRepository.java
@@ -2,9 +2,10 @@ package gmarmari.demo.microservices.orders.repositories;
 
 import gmarmari.demo.microservices.orders.entities.ProductDao;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
+@Repository
 public interface ProductRepository extends JpaRepository<ProductDao, Long> {
 
     Optional<ProductDao> findById(long productId);
